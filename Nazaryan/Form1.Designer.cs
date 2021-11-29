93207a03358153bcefa1fa80dc14a40aff5e85e2
n31349 <n31349@207-11>
2021-11-29 18:57:49 +0500

    Добавьте файлы проекта.

---
+++ Nazaryan/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Nazaryan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 466);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(837, 433);
+            this.listBox2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(294, 469);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(774, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(861, 501);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Nazaryan/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nazaryan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        abstract class Detalis
+        {
+            public int stoimost;
+            public int data;
+            public abstract void Info(ListBox l);
+            public Detalis ( int stoimostt, int dataa)
+            {
+                stoimost = stoimostt;
+                data = dataa;
+            }
+        }
+        class Procesor : Detalis
+        {
+            public int kol_yad;
+            public int kol_pot;
+            public int chastota;
+            public override void Info(ListBox l)
+            {
+                l.Items.Add($"Стоимость - {stoimost}, Год выпуска - {data}, Колличество ядер - {kol_yad}, Колличество потоков - {kol_pot}, Частота - {chastota}");
+            }
+            public Procesor(int kol_yadd, int kol_pott, int chastotaa, int stoimost, int data) : base(data, stoimost)
+            {
+                kol_yad = kol_yadd;
+                kol_pot = kol_pott;
+                chastota = chastotaa;
+            }
+        }
+        class Vidyuha : Detalis
+        {
+            public int chastotaGPU;
+            public int moshnost;
+            public int pamyat;
+            public override void Info(ListBox l)
+            {
+                l.Items.Add($"ЧастотаGPU - {chastotaGPU}, Производительность - {moshnost}, Обьем памяти - {pamyat}, Стоимость - {stoimost}, Год выпуска - {data}");
+            }
+            public Vidyuha(int chastotaGPUU, int moshnostt, int pamyatt, int stoimost, int data) : base(data, stoimost)
+            {
+                chastotaGPU = chastotaGPUU;
+                moshnost = moshnostt;
+                pamyat = pamyatt;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Procesor sot = new Procesor(16, 4, 8, 2019, 23000);
+            sot.Info(listBox2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Vidyuha sot = new Vidyuha(16, 23000, 32, 2019, 15000);
+            sot.Info(listBox2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
